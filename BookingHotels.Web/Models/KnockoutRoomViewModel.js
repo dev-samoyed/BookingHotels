c799e0c0be8b7d2c6f4676a221cd0ade77e982f2
smutkin <smutkin@outlook.com>
2017-08-16 02:53:41 +0700

    Implemented  all rooms  output using knockout.js

--- BookingHotels.Web/Models/KnockoutRoomViewModel.js
+++ BookingHotels.Web/Models/KnockoutRoomViewModel.js
@@ -1,27 +1,25 @@
-﻿var viewModel = function (data) {
-    if (data != null) {
-        ko.mapping.fromJS(data, { Rooms: clientMapping }, self);
-    }
-    var self = this;
-    self.Rooms = ko.observableArray([]);
-}
+﻿//var viewModel = function (data) {
+//    if (data != null) {
+//        ko.mapping.fromJS(data, { Rooms: clientMapping }, self);
+//    }
+//    var self = this;
+//    self.Rooms = ko.observableArray([]);
+//}
 
-var RoomDetail = function (data) { 
-    var self = this; 
-    if (data != null) 
-    { 
-        self.Id = ko.observable(data.ClientName); 
-        self.HotelId = ko.observable(data.Address);
-        self.RoomPrice = ko.observable(data.Phone); 
-        self.RoomType = ko.observable(data.Active);
-        self.Hotel = ko.observable(data.Status); 
-    }
-}
+//var RoomDetail = function (data) { 
+//    var self = this; 
+//    if (data != null) 
+//    { 
+//        self.Id = ko.observable(data.ClientName); 
+//        self.HotelId = ko.observable(data.Address);
+//        self.RoomPrice = ko.observable(data.Phone); 
+//        self.RoomType = ko.observable(data.Active);
+//        self.Hotel = ko.observable(data.Status); 
+//    }
+//}
 
-var clientMapping = {
-    create: function (options) {
-        return new RoomDetail(options.data);
-    }
-};
-
-//
+//var clientMapping = {
+//    create: function (options) {
+//        return new RoomDetail(options.data);
+//    }
+//};
