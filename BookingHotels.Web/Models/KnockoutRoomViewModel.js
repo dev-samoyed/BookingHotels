5ad6bc72ff0b0f0e4f063df18592654c565372f3
EgorSmutkin <smutkin@outlook.com>
2017-08-16 12:32:25 +0700

    modified ko template

--- BookingHotels.Web/Models/KnockoutRoomViewModel.js
+++ BookingHotels.Web/Models/KnockoutRoomViewModel.js
@@ -1,39 +1,25 @@
-﻿var myViewModel = {
-    personName: 'Bob',
-    personAge: 123
-};
+﻿//var viewModel = function (data) {
+//    if (data != null) {
+//        ko.mapping.fromJS(data, { Rooms: clientMapping }, self);
+//    }
+//    var self = this;
+//    self.Rooms = ko.observableArray([]);
+//}
 
-ko.applyBindings(new myViewModel());
+//var RoomDetail = function (data) { 
+//    var self = this; 
+//    if (data != null) 
+//    { 
+//        self.Id = ko.observable(data.ClientName); 
+//        self.HotelId = ko.observable(data.Address);
+//        self.RoomPrice = ko.observable(data.Phone); 
+//        self.RoomType = ko.observable(data.Active);
+//        self.Hotel = ko.observable(data.Status); 
+//    }
+//}
 
-
-
-///////////////////////////////////////////
-/*
-
-var viewModel = function (data) {
-    if (data != null) {
-        ko.mapping.fromJS(data, { Rooms: clientMapping }, self);
-    }
-    var self = this;
-    self.Rooms = ko.observableArray([]);
-}
-
-var RoomDetail = function (data) { 
-    var self = this; 
-    if (data != null) 
-    { 
-        self.Id = ko.observable(data.ClientName); 
-        self.HotelId = ko.observable(data.Address);
-        self.RoomPrice = ko.observable(data.Phone); 
-        self.RoomType = ko.observable(data.Active);
-        self.Hotel = ko.observable(data.Status); 
-    }
-}
-
-var clientMapping = {
-    create: function (options) {
-        return new RoomDetail(options.data);
-    }
-};
-
-*/
+//var clientMapping = {
+//    create: function (options) {
+//        return new RoomDetail(options.data);
+//    }
+//};
