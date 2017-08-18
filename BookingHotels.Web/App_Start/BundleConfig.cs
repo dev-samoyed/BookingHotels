be93e1a7790020a42de8170c88b47be07fe6e5f8
EgorSmutkin <smutkin@outlook.com>
2017-08-18 15:33:49 +0700

    datapicker

--- BookingHotels.Web/App_Start/BundleConfig.cs
+++ BookingHotels.Web/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace BookingHotels.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -29,6 +30,7 @@ namespace BookingHotels.Web
                       "~/Content/bootstrap.css",
                       "~/Content/bootstrap-select.css",
                       "~/Content/slider.css",
+                      "~/Content/themes/base/jquery-ui.css",
                       "~/Content/site.css"));
             bundles.Add(new ScriptBundle("~/bundles/knockout.js").Include(
                     "~/Scripts/knockout-3.4.2.js"));
