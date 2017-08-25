a28ebb918272e3faa893672ca56bf2a9fd830539
EgorSmutkin <smutkin@outlook.com>
2017-08-25 15:43:40 +0700

    carousel fix

--- BookingHotels.WebAPI/Controllers/ImageController.cs
+++ BookingHotels.WebAPI/Controllers/ImageController.cs
@@ -11,25 +11,17 @@ using System.Collections.Generic;
 
 namespace BookingHotels.WebAPI
 {
-
     [RoutePrefix("api/image")]
     public class ImageController : ApiController
     {
         public string imagesRootPath = Path.GetFullPath(Path.Combine(
             AppDomain.CurrentDomain.BaseDirectory, @"..\..\Images\"));
-
-
         // GET api/image
         public HttpResponseMessage Get(string roomId, [FromUri] List<string> imageIds)
         {
             if (!imageIds.Contains(null))
             {
-                //Debug.WriteLine( "recieved a list with length: " + imageIds.Length);
-                Debug.WriteLine("requestedRoom: " + roomId);
-                Debug.WriteLine("requestedImages: " + imageIds.ToString());
-            
-
-                // Get path (console application)
+                // Get app path (console application)
                 string roomImagesPath = Path.GetFullPath(Path.Combine(imagesRootPath, roomId));
                 // Set response content as string array of files paths:
                 string[] filePaths = new string[imageIds.Count];
@@ -51,7 +43,7 @@ namespace BookingHotels.WebAPI
         [Route("api/Image/Upload")]
         public IHttpActionResult UploadPicture([FromBody] RoomImageUploadModel model)
         {
-            // model.Image contains byte[]
+            // Check if model.Image contains byte[]
             if (model.Image!=null)
             {
                 // Generate Guid as new image name in UploadResult constructor
@@ -64,12 +56,8 @@ namespace BookingHotels.WebAPI
                     FileMode.Append, FileAccess.Write));
                 fs.Write(model.Image);
                 fs.Close();
-                string message1 = "===\n Image uploaded, new image name: " + imageUploadResult.Id.ToString();
-                Debug.WriteLine(message1);
-
-                //return Request.CreateErrorResponse(HttpStatusCode.Created, imageName);
+                
                 MediaTypeFormatter bsonFormatter = new BsonMediaTypeFormatter();
-                //return Request.CreateResponse<ImageUploadResult>(HttpStatusCode.Created, imageUploadResult, bsonFormatter);
                 return Ok(imageUploadResult);
             }
             else
@@ -78,24 +66,14 @@ namespace BookingHotels.WebAPI
             }
         }
 
-        // PUT api/image/5 
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        //// PUT api/image/5 
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
 
-        // DELETE api/image/5 
-        public void Delete(int id)
-        {
-        }
+        //// DELETE api/image/5 
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
-
-
-
-//// Send content as ByteArrayContent (slower):
-//FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
-//Image image = Image.FromStream(fileStream);
-//MemoryStream memoryStream = new MemoryStream();
-//image.Save(memoryStream, ImageFormat.Jpeg);
-//result.Content = new ByteArrayContent(memoryStream.ToArray());
-//result.Content.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
